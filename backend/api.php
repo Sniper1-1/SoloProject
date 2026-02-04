e5e1f120ec9a27e412f6d28c9f7bf6ace3860173
Justin Hooker <justinhooker1@gmail.com>
2026-02-03 20:06:45 -0500

    works on Netlify
    (assuming downloaded and backend run on localhost through XAMPP)

--- backend/api.php
+++ backend/api.php
@@ -1,7 +1,15 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+    header('Access-Control-Allow-Headers: Content-Type');
+    http_response_code(200);
+    exit;
+}
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 
 $DATA_FILE = "data.json";
